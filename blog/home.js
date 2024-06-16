13d001a0c051ece6cc010d8f621e8804ed1024de
CHSherr <110822398+CHSherr@users.noreply.github.com>
2024-06-15 20:23:34 -0400

    Feature 3: Delete the element through a button onclick event, also modified the previous add function to create a div containing a p and button instead of just p

--- blog/home.js
+++ blog/home.js
@@ -1,9 +1,26 @@
-function myFunction() {
-  const srcText = document.getElementById("input").innerHTML;
+function add() {
+  const newDiv = document.createElement("div");
+  newDiv.className = "teko-test";
   const newParagraph = document.createElement("p");
+  const srcText = document.getElementById("input").innerHTML;
   newParagraph.innerHTML = srcText;
   newParagraph.classList.add("teko-p");
+  const newButton = document.createElement("button");
+  newButton.innerHTML = "-";
+  newButton.classList.add("teko-button-delete");
+  newButton.setAttribute("onclick", "deletion(this)");
+
+  newDiv.appendChild(newParagraph);
+  newDiv.appendChild(newButton);
+
   const targetElement = document.getElementById("input");
   const parentElement = targetElement.parentNode;
-  parentElement.insertBefore(newParagraph, targetElement);
+  parentElement.insertBefore(newDiv, targetElement);
+}
+
+function deletion(button) {
+  const targetElement = button.parentElement;
+  if (targetElement) {
+    targetElement.remove();
+  }
 }
